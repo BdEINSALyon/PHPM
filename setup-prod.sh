79538f34235d44f5992f7d8b45baf05c54c198a8
Nornort <francois.lalleve@laposte.net>
2018-05-12 00:33:40 +0200

    Better!

--- setup-prod.sh
+++ setup-prod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cp -f /assets/app/config/parameters.yml /var/www/symfony/app/config/
 php /var/www/symfony/app/console doctrine:schema:update --force
-cp -rf * /assets/
+cp -rf /var/www/symfony/ /assets/
