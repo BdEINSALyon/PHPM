ab50e2dd734ff7456e8dc7a70c4914983910ee79
Nornort <francois.lalleve@laposte.net>
2018-05-12 01:48:51 +0200

    Better!

--- setup-prod.sh
+++ setup-prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod -R 777 /var/www/symfony/app
 cp -f /assets/app/config/parameters.yml /var/www/symfony/app/config/
 php /var/www/symfony/app/console doctrine:schema:update --force
 cp -rf /var/www/symfony/ /assets/
